103b430cbe6b676685215c8b6fb9a88891c50420
Punit Rathore <punitr@gmail.com>
2018-02-22 16:56:31 -0800

    adding new todo feature

--- src/components/app.js
+++ src/components/app.js
@@ -3,15 +3,30 @@ import TodosList from './todos_list'
 import NewTodo from './new_todo'
 
 export default class App extends Component {
-  render() {
-    let todos = {
-      1: {body: 'Buy Groceries', isComplete: false, id: 1},
-      2: {body: 'Pick up kids', isComplete: false, id: 2}
+  constructor(props) {
+    super(props)
+    this.state = {
+      todos: {
+        1: {body: 'Buy Groceries', isComplete: false, id: 1},
+        2: {body: 'Pick up kids', isComplete: false, id: 2}
+      },
+      lastTodoId: 2
     }
+    this.addTodo = this.addTodo.bind(this)
+  }
+
+  addTodo(todoBody) {
+    const todoId = this.state.lastTodoId + 1
+    const newTodo = {body: todoBody, isComplete: false, id: todoId}
+    const todos = Object.assign({}, this.state.todos, {[todoId]: newTodo})
+    this.setState({todos, lastTodoId: todoId})
+  }
+
+  render() {
     return (
       <div>
-        <TodosList todos={todos} />
-        <NewTodo />
+        <TodosList todos={this.state.todos} />
+        <NewTodo onFormSubmit={this.addTodo} />
       </div>
     );
   }
--- src/components/new_todo.js
+++ src/components/new_todo.js
@@ -1,9 +1,38 @@
 import React, { Component } from 'react'
 
 export default class NewTodo extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {body: ''}
+    this.onInputChange = this.onInputChange.bind(this)
+    this.onFormSubmit = this.onFormSubmit.bind(this)
+  }
+
+  onInputChange(event) {
+    console.log('called onInputChange')
+    this.setState({body: event.target.value})
+  }
+
+  onFormSubmit(event) {
+    event.preventDefault()
+    console.log('submitting form', this);
+    this.props.onFormSubmit(this.state.body)
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.setState({body: ''})
+  }
+
   render() {
+    console.log('renrendering NewTodo')
     return (
-      <div>This is NewTodo component</div>
+      <form onSubmit={this.onFormSubmit}>
+        <input name="body"
+               value={this.state.body}
+               type="text"
+               onChange={this.onInputChange} />
+        <button className="btn btn-primary">Create!</button>
+      </form>
     )
   }
 }
